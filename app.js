8d1a94a8d2c93c66b687b6ff3c812207939e8b52
CharlesJonah <mutua.charles48@gmail.com>
2017-07-06 16:27:42 +0300

    Initial Commit

--- app.js
+++ app.js
@@ -32,6 +32,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //Create a new users
 apiRoutes.post('/register', function (req, res) {

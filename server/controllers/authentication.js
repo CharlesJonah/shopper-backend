51dc32fe9d24355d610b3721096ac851b575423b
CharlesJonah <mutua.charles48@gmail.com>
2017-07-07 12:13:06 +0300

    bg(authentication): Fixes password validation

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -62,44 +62,49 @@ module.exports = {
         }
     },
     register(user, res) {
-        if (user.email && user.first_name && user.last_name && user.password && user.country && user.phone) {
+        if (user.email && user.firstName && user.lastName && user.password && user.country && user.phoneNumber && user.repeatPassword) {
             var email_filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
             var password_filter = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/
             if (email_filter.test(user.email)) {
-                if (password_filter.test(user.password)) {
-                    User.findOne({ email: user.email }, function (err, currentUser) {
+                if (user.password === user.repeatPassword)
+                    if (password_filter.test(user.password)) {
+                        User.findOne({ email: user.email }, function (err, currentUser) {
 
-                        if (err) {
-                            res.status(500).send({ success: false, message: 'Server encountered problem while saving.' });
-                        }
-                        if (!currentUser) {
-                            const newUser = new User(user);
-                            const hashedPassword = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
-                            newUser.password = hashedPassword;
-                            newUser.save(function (err) {
-                                if (err) {
-                                    res.status(500).send({ success: false, message: 'Server encountered problem while saving.' });
-                                }
-                                else {
-                                    res.status(201).send({ success: true, message: 'Registered successfully' });
-                                }
-                            });
-                        } else if (currentUser) {
-                            res.status(409).send({ success: false, message: 'User already exists' });
-                        }
+                            if (err) {
+                                res.status(500).send({ success: false, message: 'Server encountered problem while saving.' });
+                            }
+                            if (!currentUser) {
+                                const newUser = new User(user);
+                                const hashedPassword = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+                                newUser.password = hashedPassword;
+                                newUser.save(function (err) {
+                                    if (err) {
+                                        res.status(500).send({ success: false, message: 'Server encountered problem while saving.' });
+                                    }
+                                    else {
+                                        res.status(201).send({ success: true, message: 'Registered successfully' });
+                                    }
+                                });
+                            } else if (currentUser) {
+                                res.status(409).send({ success: false, message: 'User already exists' });
+                            }
 
-                    });
-                }
-                else {
-                    res.status(400).send({ success: false, message: 'Weak password. password should contain at least 1 lowercase alphabetical character,at least 1 uppercase alphabetical character,at least 1 numeric character and must be must be eight characters or longer.' });
-                }
+                        });
+                    }
+                    else {
+                        res.status(400).send({ success: false, message: 'Weak password. password should contain at least 1 lowercase alphabetical character,at least 1 uppercase alphabetical character,at least 1 numeric character and must be must be eight characters or longer.' });
+                    }
             }
             else {
-                res.status(400).send({ success: false, message: 'Bad Request. Invalid email' });
+
             }
         }
         else {
-            res.status(400).send({ success: false, message: 'Bad Request. Fill all the fields' });
+            res.status(400).send({ success: false, message: 'Bad Request. Invalid email' });
         }
     }
+        else {
+        res.status(400).send({ success: false, message: 'Bad Request. Fill all the fields' });
+    }
+}
 }
